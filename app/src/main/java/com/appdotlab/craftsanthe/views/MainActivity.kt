fcecdee36f1646c692f49a59e41a6ce79b1ce4e1
Abdul Jaseem <jaseemabdul20@gmail.com>
2020-08-17 11:48:08 +0530

    Init

---
+++ app/src/main/java/com/appdotlab/craftsanthe/views/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.appdotlab.craftsanthe.views
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.appdotlab.craftsanthe.R
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
